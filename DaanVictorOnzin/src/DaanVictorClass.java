e88bb218834eba2997e90013720097d2f8c0e7d3
VictorWissink <victor-wissink@hotmail.com>
2014-11-24 11:55:11 +0100

    major bug fixes

--- DaanVictorOnzin/src/DaanVictorClass.java
+++ DaanVictorOnzin/src/DaanVictorClass.java
@@ -4,11 +4,14 @@ public class DaanVictorClass {
 	
 	private int victor;
 	private int daan;
+	private int ditwordtnietgebruikt;
 	
 	
 	public void setDaanVic(int i , int j){
 		victor = j;
 		daan = i;
+		
+		
 	}
 	
 	
