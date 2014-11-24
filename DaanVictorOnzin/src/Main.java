d35705da4007939adb4f683b8e086b7256d7723b
VictorWissink <victor-wissink@hotmail.com>
2014-11-24 12:01:05 +0100

    df

---
+++ DaanVictorOnzin/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+	private static int victor;
+	private static int daan;
+	private static int ditwordtnietgebruikt;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		setDaanVic(32, 23);
+		System.out.println("49349293923");
+	}
+
+	
+	public static void setDaanVic(int i , int j){
+		victor = j;
+		daan = i;
+		ditwordtnietgebruikt = 2399;
+		
+	}
+}
